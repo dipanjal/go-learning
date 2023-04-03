4cf3a230dbf7b3a9b1b889edc3c10878e09ab654
dipanjal <dipanjalmaitra@gmail.com>
2023-04-03 19:16:20 +0600

    slice iteration

--- go-basics/slice/main.go
+++ go-basics/slice/main.go
@@ -12,4 +12,18 @@ func main() {
 	booksModified := append(books, "Go in Action")
 	fmt.Println(booksModified)
 	fmt.Printf("Reference after append %p\n", &booksModified)
+
+	// print a slice using for loop,
+	// here we are going to get a tuple like
+	// the first one is the index of the item and
+	// the second one is the item inside the slice
+	for index, item := range booksModified {
+		fmt.Println(index, "->", item)
+	}
+
+	// but what if we don't want to use the index
+	// we can just ignore it using low dash like this _
+	for _, item := range booksModified {
+		fmt.Println(item)
+	}
 }
