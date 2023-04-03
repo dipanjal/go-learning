e32631f294e9e8f28906efe9a4fda615eb830eaa
dipanjal <dipanjalmaitra@gmail.com>
2023-04-03 16:19:56 +0600

    slice works

--- go-basics/slice/main.go
+++ go-basics/slice/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	books := []string{"Java", "Python"}
+	fmt.Println(books)
+	fmt.Printf("Reference before append %p\n", &books)
+
+	// append doesn't modify the slice,
+	//instead it creates a new slice which we must to re-assign
+	booksModified := append(books, "Go in Action")
+	fmt.Println(booksModified)
+	fmt.Printf("Reference after append %p\n", &booksModified)
 }
