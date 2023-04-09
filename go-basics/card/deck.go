f96214a243664cb94d7a7174eba57cd16f23bbc0
dipanjal <dipanjalmaitra@gmail.com>
2023-04-09 13:11:46 +0600

    code-refactoring

--- go-basics/card/deck.go
+++ go-basics/card/deck.go
@@ -12,8 +12,22 @@ import (
 // Deck is a decorated type of []string slice
 type Deck []string
 
+var cardSuits = []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+var cardValues = []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
+
 const DirFixed = "./decks"
 const Separator = ","
+
+// newDeck function creates a new Decl which is a new slice of cards
+func newDeck() Deck {
+	cards := Deck{}
+	for _, suit := range cardSuits {
+		for _, val := range cardValues {
+			cards = append(cards, val+" of "+suit)
+		}
+	}
+	return cards
+}
 
 // a receiver function which can be used by any type of Deck
 // the argument deck is like this in java or self in python
--- go-basics/card/main.go
+++ go-basics/card/main.go
@@ -5,12 +5,8 @@ import (
 	"log"
 )
 
-var cardSuits = []string{"Spades", "Hearts", "Diamonds", "Clubs"}
-
-var cardValues = []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
-
 func main() {
-	myDeck := createADeck()
+	myDeck := spawnADeck()
 	myDeck.print()
 
 	fmt.Println("..... Cards after shuffle .....")
@@ -27,31 +23,21 @@ func main() {
 
 // try to create a deck from existing file
 // is the file is not present, it will create a new deck and save into a file
-func createADeck() Deck {
+func spawnADeck() Deck {
 	fileName := "my-cards.txt"
 	err, deck := createNewDeckFromFile(fileName)
 	if deck != nil {
 		return deck
 	}
 	log.Printf("Unexpected error occurred while reading file: %s\n", err)
-	return createAndSaveDeck(fileName)
+	return createNewDeckAndSaveIntoFile(fileName)
 }
 
-func createAndSaveDeck(fileName string) Deck {
+func createNewDeckAndSaveIntoFile(fileName string) Deck {
 	newDeck := newDeck()
 	err := newDeck.saveToFile(fileName)
 	if err != nil {
 		log.Printf("Unexpected error occurred while saving deck into file %s\n", err)
 	}
 	return newDeck
-}
-
-func newDeck() Deck {
-	cards := Deck{}
-	for _, suit := range cardSuits {
-		for _, val := range cardValues {
-			cards = append(cards, val+" of "+suit)
-		}
-	}
-	return cards
 }
